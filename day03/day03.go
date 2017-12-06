d912a807c59eb08deb7be001fc019f2feff9d83a
Jeremy <jmullen@aiminspections.com>
2017-12-06 11:58:43 -0500

    Complete day 3 part 2

--- day03/day03.go
+++ day03/day03.go
@@ -8,7 +8,14 @@ import (
 	"math"
 )
 
+type Coord struct {
+	x, y int
+}
+
+var spiral = make(map[Coord]int)
+
 func main() {
+
 	content, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -21,8 +28,11 @@ func main() {
 
 	var steps int
 	var turns int
-	var posX float64
-	var posY float64
+	var posX int
+	var posY int
+	var largest int
+	var part2 int
+	part2Answered := false
 
 	for steps < n-1 {
 		length := (turns / 2) + 1
@@ -30,6 +40,14 @@ func main() {
 			if steps == n-1 {
 				break
 			}
+
+			if i == 0 {
+				spiral[Coord{0, 0}] = 1
+			}
+
+			largest = CalculateValue(posX, posY)
+			spiral[Coord{posX, posY}] = largest
+
 			steps++
 			direction := turns % 4
 			switch direction {
@@ -42,10 +60,33 @@ func main() {
 			default:
 				posY--
 			}
+
+			if largest > n && !part2Answered {
+				part2Answered = true
+				part2 = largest
+			}
+
 		}
 
 		turns++
 	}
 
-	fmt.Println("Part 1:", math.Abs(posX)+math.Abs(posY))
+	fmt.Println("Part 1:", math.Abs(float64(posX))+math.Abs(float64(posY)))
+	fmt.Println("Part 2:", part2)
+}
+
+func CalculateValue(x, y int) int {
+	var total int
+
+	total = spiral[Coord{x, y}] +
+		spiral[Coord{x + 1, y}] +
+		spiral[Coord{x + 1, y + 1}] +
+		spiral[Coord{x, y + 1}] +
+		spiral[Coord{x - 1, y + 1}] +
+		spiral[Coord{x - 1, y}] +
+		spiral[Coord{x - 1, y - 1}] +
+		spiral[Coord{x, y - 1}] +
+		spiral[Coord{x + 1, y - 1}]
+
+	return total
 }
