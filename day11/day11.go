caaaa1adf2736f12632e63eee988601fc23b5615
Jeremy <jmullen@aiminspections.com>
2017-12-23 15:59:49 -0500

    Complete day11 part 2

--- day11/day11.go
+++ day11/day11.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"strings"
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -14,6 +15,43 @@ func main() {
 	}
 
 	input := strings.Split(string(content), ",")
+	x := 0
+	y := 0
+	var distance float64
+	var furthest float64
 
-	fmt.Println(input)
+	// Reference for axial coordinates https://www.redblobgames.com/grids/hexagons/
+	// Under the Axial Coordinates section
+	for _, direction := range input {
+		switch direction {
+		case "n":
+			y--
+
+		case "ne":
+			x++
+			y--
+
+		case "se":
+			x++
+
+		case "s":
+			y++
+
+		case "sw":
+			x--
+			y++
+
+		case "nw":
+			x--
+		}
+
+		distance = (math.Abs(float64(x)) + math.Abs(float64(y)) + math.Abs(float64(x)+float64(y))) / 2
+
+		if distance > furthest {
+			furthest = distance
+		}
+	}
+
+	fmt.Println("Part 1:", distance)
+	fmt.Println("Part 2:", furthest)
 }
